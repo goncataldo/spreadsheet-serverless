f1d91aaa0ee2b9ccd56cda29ff0155677df35547
goncataldo <gonzalo.cataldo@arbusta.net>
2023-02-08 13:31:31 -0300

    optimization, token now saved on json file

--- README.md
+++ README.md
@@ -2,14 +2,14 @@
 2. go to https://console.cloud.google.com/apis/credentials and select "+ CREATE CREDENTIALS" > "Service account"
 3. once the service acount is created, go to "KEYS" and create a new key, a json file should be downloaded.
 4. save the information in "client_email" and "private_key" for the .env.yml
-6. create a google spreadsheet and save the ID from the url.
-5. create an .env.yml like the example and complete it with the information saved from the json and the spreadsheet ID
+5. create a google spreadsheet and make it editable for the "client_email" saved from the json file and save the ID from the url.
+6. create an .env.yml like the example and complete it with the information saved from the json and the spreadsheet ID
 7. make sure to write the row headers on the spreadsheet, example: <br>
 ![example](https://gcdnb.pbrd.co/images/QWm9nzoQ9lk8.png?o=1)
-6. "npm i" on terminal
-7. go to https://console.developers.google.com/apis/api/sheets.googleapis.com/overview?project=634792790969 and accept spreadsheets permission 
-8. "sls offline" on terminal
-9. to verify that everything is working, insert this on terminal (dont forget to complete with the post endpoint):
+8. "npm i" on terminal
+9. go to https://console.developers.google.com/apis/api/sheets.googleapis.com/overview and accept spreadsheets permission 
+10. "sls offline" on terminal
+11. to verify that everything is working, insert this on terminal (dont forget to complete with the post endpoint):
 ```
 curl --location --request POST 'https://<INSERT-POST-ENDPOINT-HERE>' \
 --header 'Content-Type: application/json' \
--- src/handler.js
+++ src/handler.js
@@ -1,21 +1,60 @@
+
+const fs = require('fs').promises;
+const path = require('path');
+const process = require('process');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
+const { existsSync } = require('fs');
 const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+
+
+
+ async function authenticate(doc) {
+  if(existsSync(TOKEN_PATH)) { //aca convierto doc en doc autenticado con el token que tengo en token_path
+    const content = await fs.readFile(TOKEN_PATH);
+    JSON.parse(content)
+    doc = content
+    console.log("using token")
+  } else {
+    spreadsheetAuth(doc)
+    const payload = JSON.stringify(doc)
+    await fs.writeFile(TOKEN_PATH, payload);
+    console.log("new token created")
+    return doc
+  }
+}
+ 
 
 module.exports.write = async event => {
-  if(!event.body) {
-    return formatResponse(400, { message: 'body is missing' });
-  }
+  try{
+    if(!event.body) {
+      return formatResponse(400, { message: 'body is missing' });
+  } 
+
   const body = JSON.parse(event.body);
-
-  await spreadsheetAuth(doc);
-  await doc.loadInfo();
-  const sheet = doc.sheetsByIndex[0];
-
-
-  await sheet.addRow(body.row);
-
-  return formatResponse(200, { message: 'New row added'});
   
+  authenticate(doc).then(()=>{
+    doc.loadInfo()
+    .then(()=>{
+      doc.sheetsByIndex[0].addRow(body.row);
+    })
+     .catch(()=>{
+      console.log("token expired, creating new one")
+      fs.unlink(TOKEN_PATH).then(()=>{
+        authenticate(doc).then(()=>{
+          doc.loadInfo()
+          .then(()=>{
+            doc.sheetsByIndex[0].addRow(body.row);
+          })
+        })
+      })
+    }) 
+  })
+  
+  return formatResponse(200, { message: 'New row added'});
+  } catch (error){
+    return formatResponse(400, { message: 'error'});
+  }
 };
 
 function spreadsheetAuth(document) {
